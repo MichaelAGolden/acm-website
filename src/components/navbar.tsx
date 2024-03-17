7b610a4ce4f4cc10f05be870368de6eb9c36348a
Michael Golden <MichaelAGolden@users.noreply.github.com>
2024-03-17 17:48:50 -0500

    Update navbar and theme toggle styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,19 +8,18 @@ export default function Navbar() {
     const { theme } = useTheme();
 
     return (
-        <header className="sticky top-0 z-50 w-full border-b border-border/40 backdrop-blur-lg supports-[backdrop-filter]:bg-inherit/60 dark:backdrop-blur-lg
-        dark:bg-inherit/90 ">
+        <header className="sticky top-0 z-50 w-full border-b border-border/40 backdrop-blur-lg supports-[backdrop-filter]:bg-inherit/10 dark:backdrop-blur-2xl dark:border-border">
             <div className='container flex h-20 max-w-screen-2xl items-center'>
                 <button className='mr-4 flex items-center space-x-6'>
                     <Image src={`/placeholder${theme === 'dark' ? 'Dark' : 'Light'}.svg`} alt='ACM at ACC' height={64} width={64} />
                     <span className='font-sans font-bold text-lg'>ACM at ACC</span>
                 </button>
-                <div className='flex-col  md:items-center md:space-x-6 text-sm'>
+                <div className='flex-col md:items-center md:space-x-6 text-sm'>
                     <nav className='flex md:items-center gap-6 select'>
-                        <a className='transition- hover:text-foreground text-foreground/60 dark:text-background/60 dark:hover:text-foreground/80' href='#'>About Us</a>
-                        <a className='transition-colors hover:text-foreground/80 text-foreground/60 dark:text-background/60 dark:hover:text-foreground/80' href='#'>Riverhacks</a>
-                        <a className='transition-colors hover:text-foreground/80 text-foreground/60 dark:text-background/60 dark:hover:text-foreground/80' href='#'>Sponsor Us</a>
-                        <a className='transition-colors hover:text-foreground/80 text-foreground/60 dark:text-background/60 dark:hover:text-foreground/80' href='#'>Contact</a>
+                        <a className='transition-colors hover:text-foreground text-foreground/60' href='#'>About Us</a>
+                        <a className='transition-colors hover:text-foreground text-foreground/60' href='#'>Events</a>
+                        <a className='transition-colors hover:text-foreground text-foreground/60' href='#'>Resources</a>
+                        <a className='transition-colors hover:text-foreground text-foreground/60' href='#'>Contact</a>
                     </nav>
                 </div>
                 <div className='flex flex-1 items-center justify-between space-x-2 md:justify-end'>
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -17,7 +17,7 @@ export function ThemeSelector() {
 
     return (
         <DropdownMenu  >
-            <DropdownMenuTrigger className="bg-inherit border-border/0 shadow-none hover:bg-transparent/15 dark:border-border/0 dark:hover:backdrop-blur-3xl dark:hover:bg-transparent/35" asChild>
+            <DropdownMenuTrigger className="bg-inherit border-border/0 shadow-none hover:bg-popover/50 dark:border-border/0 dark:hover:backdrop-blur-3xl dark:hover:bg-transparent/35" asChild>
                 <Button variant="outline" size="icon">
                     <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -25,20 +25,20 @@ export function ThemeSelector() {
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="
-            bg-inherit/30 backdrop-blur-3xl text-popover-foreground dark:text-popover-background/60" align="end">
-                <DropdownMenuItem className="  dark:focus:bg-transparent/30
+            bg-inherit/60 backdrop-blur-3xl text-popover-foreground dark:text-popover-background/60" align="end">
+                <DropdownMenuItem className="focus:bg-popover/50 dark:focus:bg-transparent/30
                 dark:focus:backdrop-brightness-80
                 dark:focus:backdrop-blur-3xl
                 " onClick={() => setTheme("light")}>
                     Light
                 </DropdownMenuItem>
-                <DropdownMenuItem className="  dark:focus:bg-transparent/30
+                <DropdownMenuItem className="focus:bg-popover/50 dark:focus:bg-transparent/30
                 dark:focus:backdrop-brightness-80
                 dark:focus:backdrop-blur-3xl
                 "onClick={() => setTheme("dark")}>
                     Dark
                 </DropdownMenuItem>
-                <DropdownMenuItem className="  dark:focus:bg-transparent/30
+                <DropdownMenuItem className="focus:bg-popover/50 dark:focus:bg-transparent/30
                 dark:focus:backdrop-brightness-80
                 dark:focus:backdrop-blur-3xl
                 "onClick={() => setTheme("system")}>
